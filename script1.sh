af4b2a8f08cb0b14174c56825bfa95cc6256c9cc
unknown <vishalrai673@gmail.com>
2024-11-26 15:46:45 +0530

    on nov 26

--- script1.sh
+++ script1.sh
@@ -2,4 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "Now I become Death The Destroyer of both worlds"
 echo " I Just seen openhiemer"
-
+echo "I am making changes on nov 26"
